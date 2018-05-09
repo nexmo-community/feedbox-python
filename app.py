ec781bb6a6ece2213825025ea12cdf8186c628cf
Steve Crow <steve.crow@vonage.com>
2018-05-09 17:36:58 -0400

    Show a max of 5 comments.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 
 @app.route('/recent-comments-json')
 def resent_comments_json():
-    recent_comments = get_all_feedback()[:3]
+    recent_comments = get_all_feedback()[:5]
     return jsonify(extract_feedback_properties(recent_comments))
 
 
